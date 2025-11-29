1b6bf659d263aa208c092b12676a229d35449657
AyeshaSangani <aysan1004@gmail.com>
2025-11-29 13:58:19 +0500

    first data

---
+++ src/app/app-routing-module.ts
@@ -0,0 +1,20 @@
+// app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Home } from './components/pages/home/home';
+import { About } from './components/pages/about/about';
+
+
+
+const routes: Routes = [
+  { path: '', component: Home },         
+  { path: 'about', component: About}, 
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

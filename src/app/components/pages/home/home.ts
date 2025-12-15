f52a959f699b01cd745fec6d7274fc5af090338f
Ayesha Sangani <142738168+AyeshaSangani@users.noreply.github.com>
2025-12-16 04:49:46 +0500

    Add files via upload

--- src/app/components/pages/home/home.ts
+++ src/app/components/pages/home/home.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { Navbar } from '../../navbar/navbar';
 import { Banner } from './banner/banner';
 import { Cards } from './cards/cards';
+import { Footer } from "../../footer/footer";
+import { Subcriptionbanner } from "./subcriptionbanner/subcriptionbanner";
+import { Testimonials } from './testimonials/testimonials';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar,Banner,Cards],
+  imports: [Navbar, Banner, Cards, Subcriptionbanner , Testimonials , Footer],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })

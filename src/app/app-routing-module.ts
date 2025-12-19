b7977efbb705758f22f955d2c3d9e2859c276b0c
Ayesha Sangani <142738168+AyeshaSangani@users.noreply.github.com>
2025-12-19 05:40:18 +0500

    Update routing paths for About and Contact pages

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Home } from './components/pages/home/home';
 import { About } from './components/pages/about/about';
+import { Contact } from './components/pages/contact/contact';
 
 
 
 const routes: Routes = [
   { path: '', component: Home },         
-  { path: 'about', component: About}, 
+  { path: '/about-us', component: About},
+  { path: '/contact-us', component: Contact}, 
+
+
+
 
 
 ];

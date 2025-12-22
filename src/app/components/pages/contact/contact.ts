a9782b97dcff0ed2adec63c4934ab4f0b26fdb02
Ayesha Sangani <142738168+AyeshaSangani@users.noreply.github.com>
2025-12-22 22:04:51 +0500

    Add files via upload

--- src/app/components/pages/contact/contact.ts
+++ src/app/components/pages/contact/contact.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../navbar/navbar';
+import { Footer } from "../../footer/footer";
 
 @Component({
   selector: 'app-contact',
-  imports: [Navbar],
+  imports: [Navbar, Footer],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
